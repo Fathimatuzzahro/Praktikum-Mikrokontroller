f0cf34b75e870d1105a13fd56d9f95bca44e8adb
Fathimatuzzahro <70746424+Fathimatuzzahro@users.noreply.github.com>
2021-03-15 21:28:39 +0700

    percobaan 3 running LED

---
+++ sketch_mar15c/sketch_mar15c.ino
@@ -0,0 +1,71 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(13, OUTPUT);
+pinMode(12, OUTPUT);
+pinMode(11, OUTPUT);
+pinMode(10, OUTPUT);
+pinMode(6, OUTPUT);
+pinMode(5, OUTPUT);
+pinMode(4, OUTPUT);
+pinMode(3, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(13,1);//pin 13 -- on
+digitalWrite(12,1);
+digitalWrite(4,1);
+digitalWrite(3,1);
+delay(300);
+digitalWrite(13,0);//pin 13 -- off
+digitalWrite(12,0);
+digitalWrite(4,0);
+digitalWrite(3,0);
+delay(300);
+digitalWrite(11,1);
+digitalWrite(10,1);
+digitalWrite(6,1);
+digitalWrite(5,1);
+delay(300);
+digitalWrite(11,0);
+digitalWrite(10,0);
+digitalWrite(6,0);
+digitalWrite(5,0);
+delay(300);
+digitalWrite(13,1);
+digitalWrite(12,1);
+digitalWrite(11,1);
+digitalWrite(10,1);
+digitalWrite(6,1);
+digitalWrite(5,1);
+digitalWrite(4,1);
+digitalWrite(3,1);
+delay(300);
+digitalWrite(13,1);
+digitalWrite(12,1);
+digitalWrite(11,1);
+digitalWrite(10,0);
+digitalWrite(6,0);
+digitalWrite(5,1);
+digitalWrite(4,1);
+digitalWrite(3,1);
+delay(300);
+digitalWrite(13,1);
+digitalWrite(12,1);
+digitalWrite(11,0);
+digitalWrite(10,0);
+digitalWrite(6,0);
+digitalWrite(5,0);
+digitalWrite(4,1);
+digitalWrite(3,1);
+delay(300);
+digitalWrite(13,1);
+digitalWrite(12,0);
+digitalWrite(11,0);
+digitalWrite(10,0);
+digitalWrite(6,0);
+digitalWrite(5,0);
+digitalWrite(4,0);
+digitalWrite(3,1);
+delay(300);
+}
